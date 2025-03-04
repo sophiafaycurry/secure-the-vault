9fa0b267496849521fd61192ed321fdfe2377963
Sophia <sophiafaycurry@gmail.com>
2025-03-03 20:21:12 -0600

    Updated HTML file

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Pseudocode 
+1. find the secret combination
+2. hint: three answers
+*/
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+//values below
+let num1 = 12 - 2; 
+let num2 = 20 * 2;
+let num3 = 38 + 1;
+// expected outcome will equal combo
+alert(`${message} ${num1} - ${num2} - ${num3}`);
+
+
+
+
